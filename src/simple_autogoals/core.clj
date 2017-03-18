091a71c54234860e5bc8d49e59fb73d97aeef5ad
Matthew Bilyeu <bilyeu.matt@gmail.com>
2017-03-18 15:31:55 -0400

    boilerplate

---
+++ src/simple_autogoals/core.clj
@@ -0,0 +1,7 @@
+(ns simple-autogoals.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/simple_autogoals/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-autogoals.core-test
+  (:require [clojure.test :refer :all]
+            [simple-autogoals.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
