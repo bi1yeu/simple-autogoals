9ac5cf1b7950934d5e8d20e245335d1b1935bb51
Matthew Bilyeu <bilyeu.matt@gmail.com>
2017-03-18 17:04:00 -0400

    signin flow

--- src/simple_autogoals/core.clj
+++ src/simple_autogoals/core.clj
@@ -1,14 +1,32 @@
 (ns simple-autogoals.core
-  (:require [clj-http.client :as client])
+  (:require [clj-http.client :as client]
+            [clj-http.cookies :as cookies])
   (:gen-class))
 
-(defn- get-csrf []
-  (->> (client/get "https://bank.simple.com/signin")
-       :body
-       (re-find #"<meta name=\"_csrf\" content=\"(.*)\">")
-       last))
+(defn- sign-in [username password cookie-store]
+  (println "Attempting to sign-in user:" username)
+  (let [csrf (->> (client/get "https://bank.simple.com/signin"
+                              {:cookie-store cookie-store})
+                  :body
+                  (re-find #"<meta name=\"_csrf\" content=\"(.*)\">")
+                  last)]
+    (try
+      (if (-> (client/post "https://bank.simple.com/signin"
+                           {:form-params {:username username
+                                          :password password
+                                          :_csrf csrf}
+                            :cookie-store cookie-store})
+              :body
+              (.contains "Your username and passphrase don't match"))
+        (throw (Exception. "Invalid username and/or passphrase"))
+        (do (println "Successfully signed in")
+            csrf))
+      (catch Exception e
+        (println "Could not sign-in" e)
+        (throw e)))))
 
 (defn -main
   [& args]
-  (let [csrf (get-csrf)]
-    (println csrf)))
+  (let [cookie-store (cookies/cookie-store)
+        csrf (sign-in username password cookie-store)]
+    (println "Done")))
