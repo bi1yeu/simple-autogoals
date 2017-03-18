7da36d9725e61a9fcd1432d8a66ebeba7f8333ce
Matthew Bilyeu <bilyeu.matt@gmail.com>
2017-03-18 15:48:10 -0400

    get csrf token

--- src/simple_autogoals/core.clj
+++ src/simple_autogoals/core.clj
@@ -1,7 +1,14 @@
 (ns simple-autogoals.core
+  (:require [clj-http.client :as client])
   (:gen-class))
 
+(defn- get-csrf []
+  (->> (client/get "https://bank.simple.com/signin")
+       :body
+       (re-find #"<meta name=\"_csrf\" content=\"(.*)\">")
+       last))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [csrf (get-csrf)]
+    (println csrf)))
